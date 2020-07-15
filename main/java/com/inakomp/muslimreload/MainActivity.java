848477acf7c89d7d257cece1344e7d2d1c29700b
Imam Mashur <58927519+imammashur@users.noreply.github.com>
2020-07-16 01:06:48 +0800

    Add files via upload

---
+++ main/java/com/inakomp/muslimreload/MainActivity.java
@@ -0,0 +1,51 @@
+package com.inakomp.muslimreload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://cpanel-mr.inakomp.com/apk.php?id_user=0");
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new myWebclient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public class myWebclient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView wv, String url) {
+            if(url.startsWith("tel:") || url.startsWith("whatsapp:")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
